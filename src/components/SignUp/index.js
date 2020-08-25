51a9d4fe38fa6198171afce60028a2c6e1829519
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-08-25 18:07:29 +0300

    (singup) form control for button visibility

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -32,6 +32,11 @@ class SignUpForm extends Component {
 
   render() {
     const { username, email, passwordOne, passwordTwo, error } = this.state;
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === "" ||
+      email === "" ||
+      username === "";
 
     return (
       <form onSubmit={this.onSubmit}>
@@ -63,7 +68,9 @@ class SignUpForm extends Component {
           type="password"
           placeholder="Confirm Password"
         />
-        <button type="submit">Sign Up</button>
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
 
         {error && <p>{error.message}</p>}
       </form>
