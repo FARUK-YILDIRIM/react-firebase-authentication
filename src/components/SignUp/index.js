79fc3463dbc2b753c5881a722f6b616bdee85150
frkyldrm <faruk.yildirim@outlook.com.tr>
2020-08-25 17:40:20 +0300

    Created /singup Page

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-const SingUp = () => (
+import * as ROUTES from "../../constants/routes";
+
+const SignUpPage = () => (
   <div>
-    <h1>SingUp</h1>
+    <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
 
-export default SingUp;
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null,
+};
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button type="submit">Sign Up</button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+export default SignUpPage;
